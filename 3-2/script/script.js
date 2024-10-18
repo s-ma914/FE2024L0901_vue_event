c8d4e007c79fae9b06d581b045fe83414ecabc25
s-ma914 <2301376@s.asojuku.ac.jp>
2024-10-18 10:07:57 +0900

    2024/10/18

--- 3-2/script/script.js
+++ 3-2/script/script.js
@@ -1,1 +1,18 @@
-// JavaScript用ファイル
+// JavaScript用ファイル// JavaScript用ファイル
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods:{
+        handleClick(){
+            this.count++;
+        },
+        decrement(){
+            this.count--;
+        }
+    }
+
+});
